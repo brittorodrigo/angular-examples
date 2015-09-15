03219a1eec33a20aa83ebd8ba59bbb6bf8b523e2
brittorodrigo <rodrigo.debritto@gmail.com>
2015-09-15 10:13:39 -0300

    tutorial Rodrigo Branas

--- controllers.js
+++ controllers.js
@@ -1,22 +1,22 @@
 angular.module("myApp",[])
-	.controller("ListController", function  () {
-		var list = this;
-		list.people = [
-            {name:"Ana", age:"20"},
-            {name:"Maria", age:"30"},
-            {name:"David", age:"40"},
-            {name:"Peter", age:"50"},
-            {name:"Patrick", age:"60"}
-            ];
+.controller("ListController", function  () {
+  var list = this;
+  list.people = [
+  {name:"Ana", age:"20"},
+  {name:"Maria", age:"30"},
+  {name:"David", age:"40"},
+  {name:"Peter", age:"50"},
+  {name:"Patrick", age:"60"}
+  ];
 
-        list.add = function(person){
-            list.people.push(angular.copy(person));
-          };
+  list.add = function(person){
+    list.people.push(angular.copy(person));
+  };
 
-          list.remove = function(index){
-            list.people.splice(index,1);
-          };
-	});
+  list.remove = function(index){
+    list.people.splice(index,1);
+  };
+});
 
 
 
